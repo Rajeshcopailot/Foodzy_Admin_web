afc0118ae3fc8dff557c7097d31897314865d148
Rajeshkaligram <rajeshkaligram@gmail.com>
2026-03-22 16:07:51 +0530

    changes

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/api/v1',
-    // baseURL: 'http://192.168.31.173:4000/api/v1',
+    // baseURL: 'http://localhost:4000/api/v1',
+    baseURL: 'http://192.168.31.173:4000/api/v1',
     // baseURL: 'https://foodzy-backend-xskg.onrender.com/api/v1',
     timeout: 5000,
     headers: {
